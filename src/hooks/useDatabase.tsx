425b3daeaba3bd50c76b9e869e69b3b1726caf2c
Matheus Santiago <matheussilvasantiago@outlook.com>
2023-08-28 02:25:00 -0300

    optmizing functions

--- src/hooks/useDatabase.tsx
+++ src/hooks/useDatabase.tsx
@@ -3,6 +3,7 @@ import {database} from '../services/watermelon';
 import {Q} from '@nozbe/watermelondb';
 import {
   setDairies,
+  setPercentage,
   setReduxMilkPrice,
 } from '../store/redux/slices/dataBaseSlice';
 import {useAppDispatch, useAppSelector} from '../store/redux';
@@ -19,60 +20,94 @@ export default function useDatabase() {
   const {milkPrice, defaultRegion} = useAppSelector(state => state.dataBase);
   const [totalProduction, setTotalProduction] = React.useState(0);
   const dispatch = useAppDispatch();
+  const [negocitionDone, setNegocitionDone] = React.useState(0);
+  const [negocitionPending, setNegocitionPending] = React.useState(0);
+  const [negocitionRejected, setNegocitionRejected] = React.useState(0);
+
+  const getPercentageIncrease = async () => {
+    const milkPriceDB = await database.get('milk_price').query().fetch();
+    if (milkPriceDB.length < 2) return dispatch(setPercentage(0));
+
+    const sortedData = milkPriceDB.sort((a, b) => {
+      if (a._raw.year === b._raw.year) {
+        return a._raw.month - b._raw.month;
+      }
+      return a._raw.year - b._raw.year;
+    });
+
+    const lastMonth = sortedData[sortedData.length - 1]._raw;
+    const penultimateMonth = sortedData[sortedData.length - 2]._raw;
+
+    const percentageIncrease =
+      ((lastMonth.value - penultimateMonth.value) / penultimateMonth.value) *
+      100;
+
+    dispatch(setPercentage(percentageIncrease.toFixed(2)));
+  };
 
   const getMilkPrice = async () => {
     const milkPriceDB = await database.get('milk_price').query().fetch();
-    let mostRecentMonth: any = null;
-    for (const record of milkPriceDB) {
-      const recordYear = record._raw.year;
-      const recordMonth = record._raw.month;
 
-      if (!mostRecentMonth) {
-        mostRecentMonth = record;
-      } else {
-        const mostRecentYear = mostRecentMonth._raw.year;
-        const mostRecentMonthValue = mostRecentMonth._raw.month;
-
-        if (
-          recordYear > mostRecentYear ||
-          (recordYear === mostRecentYear && recordMonth > mostRecentMonthValue)
-        ) {
-          mostRecentMonth = record;
-        }
-      }
+    if (milkPriceDB.length === 0) {
+      return dispatch(setReduxMilkPrice(0));
     }
 
-    if (mostRecentMonth) {
-      const latestValue = mostRecentMonth._raw.value;
-      dispatch(setReduxMilkPrice(latestValue));
-    } else {
-      dispatch(setReduxMilkPrice(0));
-    }
+    const mostRecentMonth = milkPriceDB.reduce((mostRecent, current) => {
+      const mostRecentYear = mostRecent._raw.year;
+      const mostRecentMonth = mostRecent._raw.month;
+
+      const currentYear = current._raw.year;
+      const currentMonth = current._raw.month;
+
+      return currentYear > mostRecentYear ||
+        (currentYear === mostRecentYear && currentMonth > mostRecentMonth)
+        ? current
+        : mostRecent;
+    });
+
+    const latestValue = mostRecentMonth._raw.value;
+    dispatch(setReduxMilkPrice(latestValue));
   };
 
   const getAllProducers = async () => {
     let totalProduction = 0;
+    let negocitionDone = 0;
+    let negocitionPending = 0;
+    let negocitionRejected = 0;
+
     const allProducers = await database
       .get('producers')
       .query(
         Q.where('region', `${regions[defaultRegion - 1]?.nome.toLowerCase()}`),
       )
       .fetch();
-    const shortList = allProducers.map((producer: any) => {
-      return {
-        daily_production: producer._raw.daily_production,
-      };
+
+    allProducers.forEach((producer: any) => {
+      const {daily_production, negociation} = producer._raw;
+
+      totalProduction += daily_production;
+
+      switch (negociation) {
+        case 'done':
+          negocitionDone++;
+          break;
+        case 'in progress':
+          negocitionPending++;
+          break;
+        default:
+          negocitionRejected++;
+          break;
+      }
     });
 
-    shortList.forEach((producer: any) => {
-      totalProduction += producer.daily_production;
-    });
-
-    if (milkPrice === 0) {
-      setTotalProduction(0);
-    } else {
-      setTotalProduction(totalProduction * milkPrice);
+    if (milkPrice !== 0) {
+      totalProduction *= milkPrice;
     }
+
+    setTotalProduction(totalProduction);
+    setNegocitionDone(negocitionDone);
+    setNegocitionPending(negocitionPending);
+    setNegocitionRejected(negocitionRejected);
   };
 
   const getDairies = async () => {
@@ -89,7 +124,6 @@ export default function useDatabase() {
     } else {
       dispatch(setDairies(simplifiedData));
     }
-    console.log(simplifiedData);
   };
   useEffect(() => {
     getAllProducers();
@@ -108,5 +142,9 @@ export default function useDatabase() {
     milkPrice,
     totalProduction,
     getDairies,
+    negocitionDone,
+    negocitionPending,
+    negocitionRejected,
+    getPercentageIncrease,
   };
 }
